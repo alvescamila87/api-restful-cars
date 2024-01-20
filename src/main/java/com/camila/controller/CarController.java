cdbcb0d1b56eed0858e0799846e5352810da35d2
Camila <116912821+alvescamila87@users.noreply.github.com>
2024-01-20 13:59:40 -0300

    create postmapping at the car controller

--- src/main/java/com/camila/controller/CarController.java
+++ src/main/java/com/camila/controller/CarController.java
@@ -3,9 +3,8 @@ package com.camila.controller;
 import com.camila.model.Car;
 import com.camila.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,11 @@ public class CarController {
     @GetMapping
     public List<Car> consultar() {
         return carRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Car incluir(@RequestBody Car car) {
+        return carRepository.save(car);
     }
 }
